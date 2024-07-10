41ceff896838bb3631d4880a48371cfc87053829
Maksym Zinchenko <siqsuruq@gmail.com>
2024-07-10 00:09:27 -0100

    Custom target with simple example

---
+++ NLog.Nats.Example/NLogNatsExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using NLog;
+using NLog.Config;
+
+namespace NLogNatsExample
+{
+    internal class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Load NLog configuration from NLog.config file
+            LogManager.Configuration = new XmlLoggingConfiguration("NLog.config");
+
+            Logger.Info("This is an info message");
+            Logger.Debug("This is a debug message");
+            Logger.Error("This is an error message");
+
+            Console.WriteLine("Logs have been sent to NATS.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NLog.Targets.Nats/NLog.Targets.Nats.cs
@@ -0,0 +1,57 @@
+﻿using NATS;
+using NATS.Client.Core;
+using NLog;
+using NLog.Common;
+using NLog.Config;
+using System.Text;
+
+namespace NLog.Targets.Nats
+{
+    [Target("Nats")]
+    public class NatsTarget : TargetWithLayout
+    {
+        private NatsConnection _natsConnection;
+
+        [RequiredParameter]
+        public string NatsUrl { get; set; }
+
+        [RequiredParameter]
+        public string Subject { get; set; }
+
+        protected override void InitializeTarget()
+        {
+            base.InitializeTarget();
+            InternalLogger.Info($"Initializing NATS target with URL: {NatsUrl} and Subject: {Subject}");
+            var options = new NatsOpts { Url = NatsUrl };
+            _natsConnection = new NatsConnection(options);
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var logMessage = this.Layout.Render(logEvent);
+            InternalLogger.Info($"Publishing log message: {logMessage}");
+            SendMessageAsync(logMessage);
+        }
+
+        private async Task SendMessageAsync(string message)
+        {
+            try
+            {
+                var messageData = Encoding.UTF8.GetBytes(message);
+                await _natsConnection.PublishAsync(Subject, messageData);
+                InternalLogger.Info($"Successfully published message: {message}");
+            }
+            catch (Exception ex)
+            {
+                InternalLogger.Error($"Failed to publish message: {ex.Message}");
+            }
+        }
+
+        protected override void CloseTarget()
+        {
+            _natsConnection?.DisposeAsync().AsTask().Wait();
+            base.CloseTarget();
+            InternalLogger.Info("NATS connection closed.");
+        }
+    }
+}
